668800f21e4c5130e37d79c6b5d4f9d8b3fb6b38
minhdtb <minhdtb.83@gmail.com>
2018-04-25 22:57:28 +0700

    update coding

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const app = express();
 const engine = require('ejs-locals');
+const createError = require('http-errors');
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
     res.locals.active = req.path.split('/')[1];
@@ -28,9 +29,7 @@ app.use(index);
 app.use(api);
 
 app.use(function (req, res, next) {
-    let err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    next(createError(404));
 });
 
 app.use(function (err, req, res) {
